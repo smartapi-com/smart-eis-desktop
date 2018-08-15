fb64bd101aaa4e58cb8ac2c944cd140b23a738fb
Jalal Kiswani <kiswanij@gmail.com>
2018-08-15 01:50:06 -0700

    .

--- src/main/java/com/jalalkiswani/desktop/example/Startup.java
+++ src/main/java/com/jalalkiswani/desktop/example/Startup.java
@@ -1,9 +1,11 @@
-package com.jalalkiswani.example;
+package com.jalalkiswani.desktop.example;
 
 import com.jk.desktop.JKDesktopApplication;
+import com.jk.util.JK;
 
 public class Startup {
 	public static void main(String[] args) {
+		System.out.println(JK.getAppName());
 		JKDesktopApplication.start();
 	}
 }
