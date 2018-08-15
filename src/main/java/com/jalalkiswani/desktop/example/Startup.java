f256fe7c42a2d04770e8e9e8f8a425f732ab7994
Jalal Kiswani <kiswanij@gmail.com>
2018-08-15 14:35:37 -0700

    .

--- src/main/java/com/jalalkiswani/desktop/example/Startup.java
+++ src/main/java/com/jalalkiswani/desktop/example/Startup.java
@@ -1,11 +1,19 @@
 package com.jalalkiswani.desktop.example;
 
+import java.util.Properties;
+
+import com.jk.db.dataaccess.orm.hibernate.JKEntityManagerFactory;
+import com.jk.db.dynamic.meta.EntityMetaUtil;
 import com.jk.desktop.JKDesktopApplication;
+import com.jk.desktop.ui.UIPanelFactoryImpl;
 import com.jk.util.JK;
 
 public class Startup {
 	public static void main(String[] args) {
-		System.out.println(JK.getAppName());
 		JKDesktopApplication.start();
+		
+//		System.out.println(EntityMetaUtil.getEntityMeta("sec_privileges").getSource());
+//		UIPanelFactoryImpl i=new  UIPanelFactoryImpl();
+//		i.createMasterDetailWithListPanel("sec_privileges", new Properties());
 	}
 }
